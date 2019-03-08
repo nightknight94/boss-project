310012d6f5672efef34fec2e04aa719dd271a673
nightknight94 <xenofly94@gmail.com>
2019-03-08 19:26:26 +0100

    Fixed problem with cmake rebuild_cache when using toolchains

--- sources/toolchains/armv7-a.cmake
+++ sources/toolchains/armv7-a.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR armv7-a)
 
-set(TOOLCHAIN_PREFIX $ENV{SDK_PATH}/sysroots/x86_64-pokysdk-linux/usr/bin/arm-poky-linux-gnueabi/arm-poky-linux-gnueabi-)
+set(TOOLCHAIN_PREFIX $ENV{SDK_PATH}/sysroots/x86_64-pokysdk-linux/usr/bin/arm-poky-linux-gnueabi/arm-poky-linux-gnueabi- CACHE STRING "")
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
 
@@ -9,7 +9,7 @@ set(COMPILER_FLAGS " -march=armv7-a -marm -mfpu=neon  -mfloat-abi=hard -mcpu=cor
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COMPILER_FLAGS}" CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COMPILER_FLAGS}" CACHE STRING "" FORCE)
 
-set(SYSROOT $ENV{SDK_PATH}/sysroots/cortexa7hf-neon-vfpv4-poky-linux-gnueabi)
+set(SYSROOT $ENV{SDK_PATH}/sysroots/cortexa7hf-neon-vfpv4-poky-linux-gnueabi CACHE STRING "")
 
 set(CMAKE_SYSROOT ${SYSROOT})
 set(CMAKE_FIND_ROOT_PATH ${SYSROOT})
